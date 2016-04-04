68db14ba6c762038478f57d6fa32d8a50660b09c
niravshah <niravshah>
2016-04-04 16:32:15 +0000

    Updates

--- controllers/index.js
+++ controllers/index.js
@@ -1,21 +1,6 @@
-/*
- Copyright (C) 2015  PencilBlue, LLC
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 //dependencies
 var path = require('path');
+var _ = require('underscore');
 module.exports = function IndexModule(pb) {
     //pb dependencies
     var util = pb.util;
@@ -71,11 +56,14 @@ module.exports = function IndexModule(pb) {
                 if(util.isError(err)) {
                     console.log("ERROR!! Couldnt find type:mw_listing")
                 } else {
-                    cos.findByType(contactType, function(err, lstngs) {                      
+                    cos.findByType(contactType, function(err, lstngs) {                             
                         
                         var objects = {
                             logo: settings[0].logo,
-                            listings: lstngs
+                            listings: lstngs,
+                            locations:_.countBy(lstngs, "location"),
+                            departments:_.countBy(lstngs, "department"),
+                            roles:_.countBy(lstngs, "role")
                         };          
                         self.ts.registerLocal('angular_script', '');
                         self.ts.registerLocal('angular_objects', new pb.TemplateValue(pb.ClientJs.getAngularObjects(objects), false));
--- controllers/new_listing.js
+++ controllers/new_listing.js
@@ -1,37 +1,53 @@
 module.exports = function(pb) {
-
-  //PB dependencies
-  var util           = pb.util;
-  var BaseController = pb.BaseController;
-
-  // Instantiate the controller & extend from the base controller
-  var LandingPage = function(){};
-  util.inherits(LandingPage, pb.BaseController);
-
-  // Define the content to be rendered
-  LandingPage.prototype.render = function(cb) {
-    var output = {
-        content_type: 'text/html',
-        code: 200
+    //PB dependencies
+    var util = pb.util;
+    var BaseController = pb.BaseController;
+    // Instantiate the controller & extend from the base controller
+    var LandingPage = function() {};
+    util.inherits(LandingPage, pb.BaseController);
+    
+    LandingPage.prototype.init = function(props, cb) {
+        var self = this;
+        pb.BaseController.prototype.init.call(self, props, function() {
+            self.siteQueryService = new pb.SiteQueryService({
+                site: self.site
+            });
+            cb();
+        });
+    };    
+    
+    
+    // Define the content to be rendered
+    LandingPage.prototype.render = function(cb) {
+        var self = this;
+        var opts = {where: {settings_type: 'home_page'}};
+        self.siteQueryService.q('mwtheme_settings', opts, function(err, settings) {
+            var objects = {
+                logo: settings[0].logo
+            };
+            self.ts.registerLocal('angular_script', '');
+            self.ts.registerLocal('angular_objects', new pb.TemplateValue(pb.ClientJs.getAngularObjects(objects), false));
+            self.ts.load('mw_listing_new', function(err, result) {
+                if(util.isError(err)) {
+                    throw err;
+                }
+                cb({
+                    content: result
+                });
+            });
+        });
     };
-    this.ts.load('new_listing', function(error, result) {
-        output.content = result;
-        cb(output);
-    });
-  };
-
-  // Define the routes for the controller
-  LandingPage.getRoutes = function(cb) {
-    var routes = [{
-        method: 'get',
-        path: "/jobs/new",
-        auth_required: true,
-        access_level: pb.SecurityService.ACCESS_WRITER,
-        content_type: 'text/html'
-    }];
-    cb(null, routes);
-  };
-
-  //return the prototype
-  return LandingPage;
+    // Define the routes for the controller
+    LandingPage.getRoutes = function(cb) {
+        var routes = [{
+            method: 'get',
+            path: "/jobs/new",
+            auth_required: true,
+            access_level: pb.SecurityService.ACCESS_WRITER,
+            content_type: 'text/html'
+        }];
+        cb(null, routes);
+    };
+    //return the prototype
+    return LandingPage;
 };

6a201a3e06a0db795bd21f2c9cb0fa9e045d2d0c
niravshah <niravshah>
2016-04-17 10:34:23 +0000

    M

--- controllers/mw_admin/mw_config.js
+++ controllers/mw_admin/mw_config.js
@@ -3,10 +3,10 @@ module.exports = function(pb) {
     var util = pb.util;
     var BaseController = pb.BaseController;
     // Instantiate the controller & extend from the base controller
-    var LandingPage = function() {};
-    util.inherits(LandingPage, pb.BaseController);
+    var MwAdminPage = function() {};
+    util.inherits(MwAdminPage, pb.BaseController);
     
-    LandingPage.prototype.init = function(props, cb) {
+    MwAdminPage.prototype.init = function(props, cb) {
         var self = this;
         pb.BaseController.prototype.init.call(self, props, function() {
             self.siteQueryService = new pb.SiteQueryService({
@@ -18,7 +18,7 @@ module.exports = function(pb) {
     
     
     // Define the content to be rendered
-    LandingPage.prototype.render = function(cb) {
+    MwAdminPage.prototype.render = function(cb) {
         var self = this;
         var opts = {where: {settings_type: 'home_page'}};
         self.siteQueryService.q('mwtheme_settings', opts, function(err, settings) {
@@ -28,7 +28,7 @@ module.exports = function(pb) {
             };
             self.ts.registerLocal('angular_script', '');
             self.ts.registerLocal('angular_objects', new pb.TemplateValue(pb.ClientJs.getAngularObjects(objects), false));
-            self.ts.load('mw_listing_new', function(err, result) {
+            self.ts.load('admin/mw_config', function(err, result) {
                 if(util.isError(err)) {
                     throw err;
                 }
@@ -39,10 +39,10 @@ module.exports = function(pb) {
         });
     };
     // Define the routes for the controller
-    LandingPage.getRoutes = function(cb) {
+    MwAdminPage.getRoutes = function(cb) {
         var routes = [{
             method: 'get',
-            path: "/jobs/new",
+            path: "/mwadmin",
             auth_required: true,
             access_level: pb.SecurityService.ACCESS_WRITER,
             content_type: 'text/html'
@@ -50,5 +50,5 @@ module.exports = function(pb) {
         cb(null, routes);
     };
     //return the prototype
-    return LandingPage;
+    return MwAdminPage;
 };
---
+++ controllers/mw_admin/new_listing.js
@@ -0,0 +1,54 @@
+module.exports = function(pb) {
+    //PB dependencies
+    var util = pb.util;
+    var BaseController = pb.BaseController;
+    // Instantiate the controller & extend from the base controller
+    var LandingPage = function() {};
+    util.inherits(LandingPage, pb.BaseController);
+    
+    LandingPage.prototype.init = function(props, cb) {
+        var self = this;
+        pb.BaseController.prototype.init.call(self, props, function() {
+            self.siteQueryService = new pb.SiteQueryService({
+                site: self.site
+            });
+            cb();
+        });
+    };    
+    
+    
+    // Define the content to be rendered
+    LandingPage.prototype.render = function(cb) {
+        var self = this;
+        var opts = {where: {settings_type: 'home_page'}};
+        self.siteQueryService.q('mwtheme_settings', opts, function(err, settings) {
+            var objects = {
+                logo: settings[0].logo,
+                homePageSettings: {callouts: [{}, {}, {}], site:self.site}
+            };
+            self.ts.registerLocal('angular_script', '');
+            self.ts.registerLocal('angular_objects', new pb.TemplateValue(pb.ClientJs.getAngularObjects(objects), false));
+            self.ts.load('admin/mw_listing_new', function(err, result) {
+                if(util.isError(err)) {
+                    throw err;
+                }
+                cb({
+                    content: result
+                });
+            });
+        });
+    };
+    // Define the routes for the controller
+    LandingPage.getRoutes = function(cb) {
+        var routes = [{
+            method: 'get',
+            path: "/mwadmin/openings/new",
+            auth_required: true,
+            access_level: pb.SecurityService.ACCESS_WRITER,
+            content_type: 'text/html'
+        }];
+        cb(null, routes);
+    };
+    //return the prototype
+    return LandingPage;
+};
